67b2b7a1c8b0e7c7400de7f86feb7faa82e6605b
karananeja <karananeja.r@gmail.com>
2022-08-28 16:34:16 +0530

    arranged the project titles in ascending order

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,7 +10,15 @@ const Portfolio = () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setProjects(data);
+        setProjects(data.sort((a, b) => {
+          const titleA = a.title.toUpperCase(); // ignore upper and lowercase
+          const titleB = b.title.toUpperCase(); // ignore upper and lowercase
+          if (titleA < titleB) return -1;
+
+          if (titleA > titleB) return 1;
+
+          return 0;  // names must be equal
+        }));
       });
   }, []);
 
