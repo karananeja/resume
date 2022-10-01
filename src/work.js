44d3672b5a7a18166ac1a23be4bfeb8ad7f61abb
karananeja <karananeja.r@gmail.com>
2022-10-01 21:44:51 +0530

    updated about the work details

--- src/work.js
+++ src/work.js
@@ -1,8 +1,13 @@
 export default [
   {
     title: 'Game Developer',
+<<<<<<< HEAD
     location: 'Bragg Gaming, Chennai, TN IN',
     duration: 'Sep 2022 - Present',
+=======
+    location: 'Spin Games LLC, Chennai, TN, IN',
+    duration: 'Feb 2022 - Present',
+>>>>>>> 7087e0ef1c77c77a3a68b1629a8bb0412fdb6657
     description: [
       'Working as Front-End Engineer',
       'Developed Slot Games using Phaser.js (JavaScript Framework) which are played in Casino for US and EU Marketplaces',
@@ -21,7 +26,7 @@ export default [
   },
   {
     title: 'Investigations Specialist',
-    location: 'Amazon Development Centre, Bengaluru, KA IN',
+    location: 'Amazon Development Centre, Bengaluru, KA, IN',
     duration: 'Nov 2020 - Feb 2022',
     description: [
       'Worked on Mutliple SOPs to make Amazon a better place for e-commerce world for both Buyers and Sellers',
@@ -31,7 +36,7 @@ export default [
   },
   {
     title: 'Tutor',
-    location: 'Bengaluru, KA IN',
+    location: 'Bengaluru, KA, IN',
     description: [
       'Tutoring students from class VI to XII for Mathematics and Science',
       'Tutoring students for all the types of Syllabus - ICSE, CBSE and State',
