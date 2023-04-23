e615e9f16354f3c101cef3950b666583f51ea960
karananeja <karananeja.r@gmail.com>
2023-04-23 22:37:35 +0530

    updated the work details

--- src/work.js
+++ src/work.js
@@ -1,28 +1,20 @@
-export default [
+const work = [
   {
     title: 'Frontend Web Developer',
     location: 'Intellicar Telematics, Bengaluru, KA IN',
     duration: 'Jan 2023 - Present',
     description: [
       'Working as Front-End Web Developer',
-      'Developed npm package',
-      'Integrated the Back - End to the Front - End',
-    ],
-  },
-  {
-    title: 'Game Developer',
-    location: 'Bragg Gaming, Chennai, TN IN',
-    duration: 'Sep 2022 - Jan 2023',
-    description: [
-      'Worked as Front-End Engineer',
-      'Developed Slot Games using Phaser.js (JavaScript Framework) which are played in Casino for US and EU Marketplaces',
+      'Developed Inventory Management System',
+      'Developed Energy Management System',
+      'Developed NPM Package for Login Provider',
       'Integrated the Back - End to the Front - End',
     ],
   },
   {
     title: 'Game Developer',
     location: 'Spin Games LLC, Chennai, TN IN',
-    duration: 'Feb 2022 - Sep 2022',
+    duration: 'Feb 2022 - Jan 2023',
     description: [
       'Worked as Front-End Engineer',
       'Developed Slot Games using Phaser.js (JavaScript Framework) which are played in Casino for US and EU Marketplaces',
@@ -34,7 +26,7 @@ export default [
     location: 'Amazon Development Centre, Bengaluru, KA, IN',
     duration: 'Nov 2020 - Feb 2022',
     description: [
-      'Worked on Mutliple SOPs to make Amazon a better place for e-commerce world for both Buyers and Sellers',
+      'Worked on Multiple SOPs to make Amazon a better place for e-commerce world for both Buyers and Sellers',
       'Created a powerpoint presentation for easy understanding of an SOP for the Team',
       'Validate the Listing Genuinity by using In-House tools',
     ],
@@ -48,3 +40,5 @@ export default [
     ],
   },
 ];
+
+export default work;
